d8e86cb4bb7d016c3e366104310aa0f4f99c4453
Lauren Etheridge <laurenetheridge@losanity.lan>
2021-10-06 10:10:10 -0400

    add alt text w/ validation to mainImage

--- schemas/page.js
+++ schemas/page.js
@@ -7,7 +7,53 @@ export default {
             name: 'title',
             title: 'Title',
             type: 'string',
-          },
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+              source: 'title',
+              maxLength: 96,
+            },
+        },
+        {
+            name: 'mainImage',
+            title: 'Main image',
+            type: 'image',
+            description: 'Add a poster for preview cards or image related to to text.',
+            fields: [
+                {
+                  title: 'Caption',
+                  name: 'caption',
+                  type: 'string',
+                  options: {
+                    isHighlighted: true,
+                  },
+                  //hidden: true
+                },
+                {
+                  name: 'alt',
+                  type: 'string',
+                  title: 'Alt text',
+                  description: 'Alternative text for screenreaders.',
+                  validation: Rule => [
+                      Rule.required(),
+                    ],
+                  options: {
+                    isHighlighted: true,
+                  },
+                },
+            ],
+            options: {
+              hotspot: true,
+            },
+        },
+        {
+            name: 'body',
+            title: 'Body',
+            type: 'blockContent',
+        },
 
     ]
 }
--- schemas/post.js
+++ schemas/post.js
@@ -46,6 +46,30 @@ export default {
         name: 'mainImage',
         title: 'Main image',
         type: 'image',
+        description: 'Add a poster for preview cards or image related to to text.',
+        fields: [
+            {
+              title: 'Caption',
+              name: 'caption',
+              type: 'string',
+              options: {
+                isHighlighted: true,
+              },
+              //hidden: true
+            },
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alt text',
+              description: 'Alternative text for screenreaders.',
+              validation: Rule => [
+                  Rule.required(),
+                ],
+              options: {
+                isHighlighted: true,
+              },
+            },
+        ],
         options: {
           hotspot: true,
         },
