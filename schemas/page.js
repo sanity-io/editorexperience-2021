feb1c00716c1024192951bdbdd2603cae1921f3b
Knut Melvær <knut.melvaer@gmail.com>
2021-10-07 14:48:05 +0200

    add advanced validation rule

--- schemas/page.js
+++ schemas/page.js
@@ -2,19 +2,28 @@ export default {
     type: 'document',
     name: 'page',
     title: 'Page',
-    fields: [
-        {
+    fields: [{
             name: 'title',
             title: 'Title',
             type: 'string',
+            description: 'Keep the title informative. Preferably check its score in our SEO tool.',
+            validation: Rule => Rule.max(60).warning('Your title should preferably be under 60 characters for SEO')
         },
         {
             name: 'slug',
             title: 'Slug',
             type: 'slug',
+            validation: Rule => Rule.custom((slug, context) => {
+                const { document /* , parent, path, type */ } = context
+
+                if (slug.current && !document.title) {
+                    return "There's a slug, but no title 🤨"
+                }
+                return true
+            }),
             options: {
-              source: 'title',
-              maxLength: 96,
+                source: 'title',
+                maxLength: 96,
             },
         },
         {
@@ -22,31 +31,30 @@ export default {
             title: 'Main image',
             type: 'image',
             description: 'Add a poster for preview cards or image related to to text.',
-            fields: [
-                {
-                  title: 'Caption',
-                  name: 'caption',
-                  type: 'string',
-                  options: {
-                    isHighlighted: true,
-                  },
-                  //hidden: true
+            fields: [{
+                    title: 'Caption',
+                    name: 'caption',
+                    type: 'string',
+                    options: {
+                        isHighlighted: true,
+                    },
+                    //hidden: true
                 },
                 {
-                  name: 'alt',
-                  type: 'string',
-                  title: 'Alt text',
-                  description: 'Alternative text for screenreaders.',
-                  validation: Rule => [
-                      Rule.required(),
+                    name: 'alt',
+                    type: 'string',
+                    title: 'Alt text',
+                    description: 'Alternative text for screenreaders.',
+                    validation: Rule => [
+                        Rule.required(),
                     ],
-                  options: {
-                    isHighlighted: true,
-                  },
+                    options: {
+                        isHighlighted: true,
+                    },
                 },
             ],
             options: {
-              hotspot: true,
+                hotspot: true,
             },
         },
         {
