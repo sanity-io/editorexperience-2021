5949d5b68ac98a86913e570d94a40d6885ac2327
Lauren <lauren@sanity.io>
2021-10-08 09:12:55 -0400

    vendor

--- schemas/vendor.js
+++ schemas/vendor.js
@@ -39,6 +39,29 @@ export default {
       name: 'logo',
       title: 'logo',
       type: 'image',
+      fields: [
+        {
+          title: 'Caption',
+          name: 'caption',
+          type: 'string',
+          options: {
+            isHighlighted: true,
+          },
+          //hidden: true
+        },
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alt text',
+          description: 'Alternative text for screenreaders.',
+          validation: Rule => [
+              Rule.required(),
+            ],
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ],
     },
     {
       name: 'description',
